19c6700be7099bed4c1f37481ec14b65a1e2324e
arno <arno.dedonder@brainjar.ai>
2023-07-31 17:25:10 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='thundertools',
-    version='0.4.3',
+    version='0.4.4',
     author='Arno De Donder',
     author_email='arno.dedonder@hotmail.com',
     description='Different tools for working with jupuyter notebooks',
