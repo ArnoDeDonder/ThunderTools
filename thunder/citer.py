761d21ae7dd031b9f7bbd5051498f0f9a5fd5fb5
arno <arno.dedonder@brainjar.ai>
2024-02-29 15:16:07 +0100

    citer improvements

--- thunder/citer.py
+++ thunder/citer.py
@@ -1,10 +1,21 @@
+from datetime import datetime
+from time import time
 from typing import Iterable
+from statistics import mean
 
 BLOCK_SYMBOL = '█'
 NON_BLOCK_SYMBOL = '░'
 
 
+def _format_seconds_to_hms(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return "{:02d}:{:02d}:{:02d}".format(int(h), int(m), int(round(s)))
+
+
 def citer(items: Iterable, block_amount=50, iterator_log_interval=100):  # Count ITERation
+    start_time = time()
+    iteration_durations = []
     try:
         len(items)
     except TypeError:
@@ -13,12 +24,21 @@ def citer(items: Iterable, block_amount=50, iterator_log_interval=100):  # Count
                 print(f'({i} / ?)')
             yield item
         return
-    if (item_amount := len(list(items))) == 0:
+    item_amount = len(list(items))
+    if item_amount == 0:
         return
     for i, item in enumerate(items):
+        iteration_start_time = time()
         if i > 0 and i % (int(item_amount / block_amount) or 1) == 0:
             block_to_print = int(round(i / item_amount * block_amount, 0))
             non_blocks_to_print = block_amount - block_to_print
-            print(f'({str(i).ljust(len(str(item_amount)))} / {item_amount}) : '
-                  + block_to_print * BLOCK_SYMBOL + non_blocks_to_print * NON_BLOCK_SYMBOL)
+            total_iteration_time = (mean(iteration_durations) * item_amount)
+            time_elapsed = time() - start_time
+            print(
+                f'({str(i).ljust(len(str(item_amount)))} / {item_amount}) : '
+                + block_to_print * BLOCK_SYMBOL + non_blocks_to_print * NON_BLOCK_SYMBOL
+                + f' - ETA: {"∞" if not iteration_durations else (datetime.fromtimestamp(start_time + total_iteration_time)).replace(microsecond=0)}'
+                + f' [{_format_seconds_to_hms(time_elapsed)}<{_format_seconds_to_hms(total_iteration_time)}]'
+            )
         yield item
+        iteration_durations.append(time() - iteration_start_time)
